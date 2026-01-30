d0a813a54cdfc0dcf7988a2159da570007119714
leomggg <leomuga96@gmail.com>
2026-01-30 14:37:12 +0100

    Plantilla index y css home - Pending: fix navBar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,9 +6,21 @@ export default function Home() {
     <>
       <div className={styles.divPrincipal}>
         <div className={styles.divAzul}>
-          <h1>¿Qué es AllCourts?</h1>
-          <p>k dise supollika 🤫 tengo la que se llevo al dieguito papá 🇦🇷❄️💀 ahh re seco lo dejo al viejo jajajaja🤣  puro veneno blanco pa la tocha primo, nada de yeso pa parguelas 👃⚡️ te hacen par d filetazos?? 🐍🔥 espabila k me lo quitan de las manos 💸💨</p>
+          <div>
+            <h1>AllCourts</h1>
+            <p>k dise supollika 🤫 tengo la que se llevo al dieguito papá 🇦🇷❄️💀 ahh re seco lo dejo al viejo jajajaja🤣  puro veneno blanco pa la tocha primo, nada de yeso pa parguelas 👃⚡️ te hacen par d filetazos?? 🐍🔥 espabila k me lo quitan de las manos 💸💨</p>
+          </div>
+          <img className={styles.contenidoFoto} src="https://imgs.search.brave.com/qikWX7TwrQx0xFHdsNsdAnXTgDYvwe_70T9yQC3EZUU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9mb3Rv/Z3JhZmlhcy5sYXNl/eHRhLmNvbS9jbGlw/cGluZy9jbXNpbWFn/ZXMwMi8yMDI1LzEy/LzA5L0FENjUwQUU4/LUE5N0EtNEIzNy04/RDMwLTZBM0VEMDAy/RTVGMy9qb3NlLWx1/aXMtYWJhbG9zLXNh/bGUtY29uZ3Jlc28t/ZGlwdXRhZG9zLWlt/YWdlbi1hcmNoaXZv/XzE2MC5qcGc_Y3Jv/cD0zNzYxLDIxMTYs/eDAseTAmd2lkdGg9/NTQ0JmhlaWdodD0z/MDYmb3B0aW1pemU9/bG93JmZvcm1hdD13/ZWJwbHk"></img>
         </div>
+
+        <div className={styles.divBlanco}>
+          <img className={styles.contenidoFoto} src="https://imgs.search.brave.com/qikWX7TwrQx0xFHdsNsdAnXTgDYvwe_70T9yQC3EZUU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9mb3Rv/Z3JhZmlhcy5sYXNl/eHRhLmNvbS9jbGlw/cGluZy9jbXNpbWFn/ZXMwMi8yMDI1LzEy/LzA5L0FENjUwQUU4/LUE5N0EtNEIzNy04/RDMwLTZBM0VEMDAy/RTVGMy9qb3NlLWx1/aXMtYWJhbG9zLXNh/bGUtY29uZ3Jlc28t/ZGlwdXRhZG9zLWlt/YWdlbi1hcmNoaXZv/XzE2MC5qcGc_Y3Jv/cD0zNzYxLDIxMTYs/eDAseTAmd2lkdGg9/NTQ0JmhlaWdodD0z/MDYmb3B0aW1pemU9/bG93JmZvcm1hdD13/ZWJwbHk"></img>
+          <div>
+            <h1>AllCourts</h1>
+            <p>k dise supollika 🤫 tengo la que se llevo al dieguito papá 🇦🇷❄️💀 ahh re seco lo dejo al viejo jajajaja🤣  puro veneno blanco pa la tocha primo, nada de yeso pa parguelas 👃⚡️ te hacen par d filetazos?? 🐍🔥 espabila k me lo quitan de las manos 💸💨</p>
+          </div>
+        </div>
+
       </div>
     </>
   );
