0f47faba18a2b2d04c6c227f27b340234e02f549
jesusgxmez <jgomezserrano19@gmail.com>
2026-02-17 08:45:16 +0100

    feat: ADD: Cookies and Privacy Pages ADD: Footer and Footer Styles

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,17 +6,32 @@ import { useState, useEffect } from "react";
 
 export default function Footer() {
     const { t } = useTranslation();
-    const [mounted, setMounted] = useState(false);
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) return null;
 
     return (
         <footer className={styles.footer}>
-            <p>&copy; {new Date().getFullYear()} {t("footer.text")}</p>
+            <div className={styles.footerTop}>
+                <div className={styles.logoCol}>
+                    <img src="/logonegro.png" alt="AllCourts Logo" />
+                    <span className={styles.logoText}></span>
+                </div>
+
+                <div className={styles.linksCol}>
+                    <p className={styles.colTitle}>AllCourts</p>
+                    <a href="" className={styles.link}>Pistas</a>
+                    <a href="/register" className={styles.link}>Registrarse</a>
+                    <a href="/login" className={styles.link}>Iniciar Sesión</a>
+                </div>
+
+                <div className={styles.linksCol}>
+                    <p className={styles.colTitle}>Legal</p>
+                    <a href="" className={styles.link}>Política de privacidad</a>
+                    <a href="" className={styles.link}>Política de cookies</a>
+                </div>
+            </div>
+
+            <div className={styles.footerBottom}>
+                <p className={styles.copyright}>&copy; {new Date().getFullYear()} {t("footer.text")}</p>
+            </div>
         </footer>
     );
 }
